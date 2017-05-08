e5cb0608056d8e6b389c8688c4b2938ad445b3b7
Angel Hu <angelqhu513@gmail.com>
2017-05-08 13:14:04 -0500

    Add project files.

---
+++ WoofBuddy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WoofBuddy.Startup))]
+namespace WoofBuddy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
